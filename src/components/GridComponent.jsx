ad713b1dcd9953d90ee2844dc5eb4846a2038a75
Teemy17 <teemy17@gmail.com>
2024-12-04 21:50:54 +0700

    fix layout :bug:

--- src/components/GridComponent.jsx
+++ src/components/GridComponent.jsx
@@ -22,7 +22,7 @@ const GridComponent = () => {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
+      <div className="grid md:grid-cols-4 gap-5">
         {items.map((item) => (
           <div
             key={item.id}
