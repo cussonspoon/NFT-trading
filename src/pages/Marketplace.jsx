6c8e2b33fb6b0a1ba04fe8ce02f5f5481ec6bb24
Teemy17 <teemy17@gmail.com>
2024-12-04 23:40:14 +0700

    implement fetch data function

--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -29,42 +29,34 @@ const Button = ({ name, setMode, mode }) => {
   );
 };
 
-const UserSalecard = ({ width, height, img }) => {
-  return (
-    <div className="w-60 h-96 rounded-2xl relative overflow-hidden group mb-3">
-      {/* Background Image */}
-      <img
-        className="object-cover w-full h-full absolute top-0 left-0 z-0"
-        src={img || "/nft/nft1.jpg"}
-        alt="NFT"
-      />
-
-      {/* Card Details */}
-      <div className="absolute bottom-0 w-full h-28 bg-black bg-opacity-50 text-white p-4 z-10">
-        <div className="absolute bottom-8 flex flex-col">
-          <h1 className="text-xl font-bold">Citizen #941</h1>
-          <div className="flex justify-between items-center mt-2">
-            <div className="flex flex-col">
-              <h2 className="text-md font-semibold pb-3">2 ETH ($5,590)</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Buy Now Button (Hidden by Default) */}
-      <div className="absolute bottom-[-3rem] w-full bg-blue-500 text-white text-center p-2 z-20 transition-all duration-500 ease-in-out group-hover:bottom-0">
-        Sell now
-      </div>
-    </div>
-  );
-};
-
 const Marketplace = () => {
   const [mode, setMode] = useState("Collection");
+  const [nftData, setNftData] = useState([]);
   const getUser = async () => {
     const response = await axios.get("http://127.0.0.1:8000/user/me");
   };
   useEffect(() => {}, []);
+
+  // Fetch NFT data
+  // pass the data into the GridComponent and sellercard
+  useEffect(() => {
+    const fetchNFTData = async () => {
+      try {
+        const response = await fetch(""); // api endpoint here
+        {
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          const data = await response.json();
+          setNftData(data);
+        }
+      } catch (e) {
+        console.error("Error fetching data", e);
+      }
+    };
+    fetchNFTData();
+  }, []);
+
   return (
     <Layout>
       <div className="relative w-full min-h-screen bg-zinc-900 overflow-auto">
