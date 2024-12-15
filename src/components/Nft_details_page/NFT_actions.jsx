49fe23d76af36e2becd9cd3de3affd9b5aa8c7fb
Teemy17 <teemy17@gmail.com>
2024-12-15 18:22:40 +0700

    :sob: change layout + more progress in page

--- src/components/Nft_details_page/NFT_actions.jsx
+++ src/components/Nft_details_page/NFT_actions.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 const NFTActions = ({ price, onBuyNow, onMakeOffer }) => {
     return (
-        <div className="bg-gray-800 p-4 rounded-lg">
+        <div className="bg-zinc-900 p-4 rounded-lg border border-zinc-700">
             <p className="text-gray-400 text-sm">Current Price</p>
             <div className="flex items-center justify-between">
                 <p className="text-2xl font-bold">{price} USDC</p>
